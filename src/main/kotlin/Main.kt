37a0689652459a0b3dc53c759efcb337dc9d163a
Garima <garima89077@gmail.com>
2022-06-22 22:00:26 +0530

    val and var difference

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,17 @@
 fun main() {
     println("Hello World!")
+    var wholeNumber=18
+    var condition=false
+
+    val number=2345
+    val name='G'
+
+    wholeNumber =wholeNumber+5
+    print(wholeNumber)
+    print(condition)
+
+    print(number)
+    print(name)
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
