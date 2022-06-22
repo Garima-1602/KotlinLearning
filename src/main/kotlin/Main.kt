b31d212e25da41e669da42ddca9ce9d933eb48da
Garima <garima89077@gmail.com>
2022-06-22 23:53:17 +0530

    Strings and string func in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
 fun main() {
     //comparison operators in kotlin
 
-    var str="Garima Kamboj"
-    var stringlength=str.length
+    val str="Garima Kamboj"
+    val stringlength=str.length //stringlength to find length
+    val stringindex=str.get(12) //string get to get value at particular index
+    println(str)
     println(stringlength)
+    println(stringindex)
+    println("The value from ${str.subSequence(2,7)}") //exlude 7th character
+    var i=1
+    println("The value is $i")//$ it is string template which concatenate the string
 
 }
