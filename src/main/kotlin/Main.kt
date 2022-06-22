fd4a1095bcdade3fe3f1b1bb47e45fc5277bcc07
Garima <garima89077@gmail.com>
2022-06-23 00:22:36 +0530

    basic function syntax

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,9 @@
 fun main() {
-    //comparison operators in kotlin
+    newFun() //invoking a func
 
-    val str="Garima Kamboj"
-    val stringlength=str.length //stringlength to find length
-    val stringindex=str.get(12) //string get to get value at particular index
-    println(str)
-    println(stringlength)
-    println(stringindex)
-    println("The value from ${str.subSequence(2,7)}") //exlude 7th character
-    var i=1
-    println("The value is $i")//$ it is string template which concatenate the string
-
+}
+//this body in {} is known as block body
+//whereas using= instead of {} is known as expression body
+fun newFun():Unit{
+    println("This is a new function")
 }
