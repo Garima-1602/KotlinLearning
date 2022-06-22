1de0d01f032db40a2b80ca92c1f6a40c06507257
Garima <garima89077@gmail.com>
2022-06-22 22:55:57 +0530

    operators

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 fun main() {
-    println("Hello World!")
-    var wholeNumber=18
-    var condition=false
+    //operators in kotlin
+    val a=2.5
+    val b=1
+    var result:Double
 
-    val number=2345
-    val name='G'
+    result=a*b
+    println(result)
+    result=a-b
+    println(result)
+    result=a+b
+    println(result)
+    result=a/b
+    println(result)
+    result=a%b
+    println(result)
 
-    wholeNumber =wholeNumber+5
-    //types of variables are not changed
-    println(wholeNumber)
-    println(condition)
-//println is used to print in new line
-    println(number)
-    println(name)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    //println("Program arguments: ${args.joinToString()}")
+
 }
