d5a05388909a98ef916a74252994f7d1cde9cde6
Garima <garima89077@gmail.com>
2022-06-22 23:37:32 +0530

    Strings in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,8 @@
 fun main() {
     //comparison operators in kotlin
-    val a=2.5
-    val b=1.4
-    var result:Boolean
 
-   result=a>b
-    println(result)
-    result=a<b
-    println(result)
-    result= a==b
-    println(result)
-    result=a>=b
-    println(result)
-    result=a<=b
-    println(result)
-    result=a!=b
-    println(result)
+    var str="Garima Kamboj"
+    var stringlength=str.length
+    println(stringlength)
 
 }
