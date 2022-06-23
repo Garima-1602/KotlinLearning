a96528dee36b1e53ef6c476a5966376bf73cf2da
Garima <garima89077@gmail.com>
2022-06-23 18:33:39 +0530

    if else statement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,18 @@
-import java.util.*
-
-fun main() {
- var characterArray=arrayOf<Char>('g','a','r','i','m','a')
-    val charArray=arrayOf<Char>('a','e','i','o','u')
-
-    //array of string
-    var stringArray:Array<String> =arrayOf("Hello","guys","welcome")
-    println(stringArray[0])
-    stringArray[0]="Hey"
-    println(stringArray[0])
-    println(stringArray.size)//we cannot chnage size of array
-    //to print all elements of an array
-    //arrays is library which is used to perform operatiosn like tostring to conver array to string
-    //arraysto string func is string representation of array
-    println(Arrays.toString(stringArray))
-    //Define an array with the contents (1, 2, 3, 4) and print it using the Arrays.toString() function.
-    var arrayInt =arrayOf<Int>(1,2,3,4)
-    println(Arrays.toString(arrayInt))
-
+fun main()
+{//Block body
+   var a=10
+   var b=11
+   var maxValue:Int
+   if(a>b)
+   {
+      maxValue=a
+   }
+   else
+   {
+      maxValue=b
+   }
+   println(maxValue)
 }
+//expression body
+//val maxValue:Int=if(a>b){a}else{b} println(maxValue)
+
