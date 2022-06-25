18be69ff41ce77770c60aa23fabe64be5197dc14
Garima <garima89077@gmail.com>
2022-06-25 13:43:12 +0530

    when and in operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,11 @@
 fun main(){
-   //using when keyword instead of if else ladder
-   var num=0
-
-   when(num!=0){
-      num>0->println("Positive")
-      num<0->println("Negative")
-
-
-      else->println("Zero")
+   //to check variable lies in the given range -using if else/or using in operator
+   val num=15
+   when(num){
+      in 1 ..10->println("Lies between 1-10")
+      in 11..20->println("Lies between 11-20")
+      else->println("None of these")
    }
-   //when{ num>0->prinltn("Positive")......
+
+
 }
