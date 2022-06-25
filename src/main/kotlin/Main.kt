d89504e7852d8ff24f01dad26a4ad133e87c7bd9
Garima <garima89077@gmail.com>
2022-06-25 23:02:16 +0530

    traversing in array using for loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,7 @@
 fun main(){
-   //to print counting from 1 to 10 using while loop
-   var i=1
-   do
+  var a=arrayOf("a","b","c","d","e")
+   for(i in 0..a.size-1)
    {
-      //i is iterator and 1..10 is range here iterator takes the first value itself
-      //only in for loop
-      println(i)
-      i++
-   }while(i<=10)
-
+      println("${a[i]}")
+   }
 }
