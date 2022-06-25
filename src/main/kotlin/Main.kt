d88501de1be639160896dbe5f9298e696d7f5ccd
Garima <garima89077@gmail.com>
2022-06-25 22:52:24 +0530

    do while loop in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main(){
    //to print counting from 1 to 10 using while loop
    var i=1
-   while(i<=10)
+   do
    {
       //i is iterator and 1..10 is range here iterator takes the first value itself
       //only in for loop
       println(i)
       i++
-   }
+   }while(i<=10)
 
 }
