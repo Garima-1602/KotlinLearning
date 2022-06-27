17b691c63bc28b45ab66bec968f8ea33fac8ba00
Garima <garima89077@gmail.com>
2022-06-27 19:32:30 +0530

    handling error and exception

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,17 @@
 fun main(){
+    //two types of exceptions
+    //java.lang.arithmeticException
+    //ArrayIndexOutOfException
+    //NPE-Null Pointer Exception
+    //npe occur when compiler expect value of variable but get null
+    //null safety operator-string?.length
+    //elvis operator -to print default value instead of null
+    //filterNotNull()used to remove null elemnets from array or list
 
-    //exception handling -using try,catch,finally
-    val d= arrayOf(3,4,5,6)
-    try{
-        d[4]=7
-        println("i ran and catch the value which cause error exception")
-        //since there is an exception the print statement will not executed
-    }catch(e: ArrayIndexOutOfBoundsException){
-          println("catch the exception")
-    }finally{//finally keyword is optional
-        println(d[2])
-    }
-
+    //var nullValue:String?=null
+    //println(nullValue?.length?:"This is null") //?:elvis operator //?.nullsafety operator
+    val nullList=listOf(1,2,3,null,5,null,6,null,0)
+    var arrayList=arrayListOf("This","is",null,"a","statement",null)
+    println("The list without null value is ${nullList.filterNotNull()}")
+    println("The array without null value is ${arrayList.filterNotNull()}")
 }
