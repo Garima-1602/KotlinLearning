44599d79efa36da78a5711f2fe7f8cde88b19b08
Garima <garima89077@gmail.com>
2022-06-27 17:09:56 +0530

    immutable list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main(){
    //arrays are fixed
-// lists are changeable-mutable and unmutable
+// lists are changeable-mutable and immmutable
     //syntax for declaration-var/val listname=listOf<datatype>(list of items)
+    //immutable list used to store static values/information
     val listOfString= listOf("garima","is","learning","kotlin")
     print(listOfString)
 }
