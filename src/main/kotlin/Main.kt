d122d4d5461257f3f66a499e3799d7858d97a9f0
Garima <garima89077@gmail.com>
2022-06-27 17:34:01 +0530

    mutable list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main(){
    //mutable lists are changeable
     //syntax for defining:-var/val listname =mutableListOf<data type>(list of items)
-    var listOfString= mutableListOf("garima","is","learning","kotlin")
+    val listOfString= mutableListOf("garima","is","learning","kotlin")
     listOfString[0]="GK"
+    listOfString.add("daily")
+    listOfString.removeAt(3)
+    val arr=arrayOf("a,","b","c")
+    listOfString.addAll(arr)
     print(listOfString)
 }
