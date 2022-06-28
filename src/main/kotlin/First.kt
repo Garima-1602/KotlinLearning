f88994d4f876ea8e0e9122b79283d00228af2dea
Garima <garima89077@gmail.com>
2022-06-28 17:57:43 +0530

    constructors

--- src/main/kotlin/First.kt
+++ src/main/kotlin/First.kt
@@ -1,25 +1,18 @@
-class First {
-    var breed:String="Stephard"
-    var color:String="Brown"
-    var age:Int=5
-    fun eat()
+import com.sun.tools.attach.AgentInitializationException
+
+class First{
+    var breed:String
+    var color:String
+    var age:Int
+    constructor(breed:String,color:String,age:Int)
     {
-        println("${breed}s eat food")
+        this.breed=breed
+        this.color=color
+        this.age=age
     }
-    fun bark()
-    {
-        println("${breed}s bark")
-    }
+
 }
-//call the func by creating object
 fun main()
 {
-    val f1=First()
-    //here f1 is object
-    f1.breed="German Stephard" //accesing the varaibvles using object
-    f1.color="Black"
-    f1.age=1
-    println("The breed of the dog is ${f1.breed},whose color is ${f1.color} ,and age is ${f1.age}")
-    f1.eat()
-    f1.bark()
+
 }
