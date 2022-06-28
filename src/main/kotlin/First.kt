9393412339201b3e4e9a8e3b27dc625879850b77
Garima <garima89077@gmail.com>
2022-06-28 11:42:09 +0530

    creating classes and objects

---
+++ src/main/kotlin/First.kt
@@ -0,0 +1,24 @@
+class First {
+    var breed:String="Stephard"
+    var color:String="Brown"
+    var age:Int=5
+    fun eat()
+    {
+        println("${breed}s eat food")
+    }
+    fun bark()
+    {
+        println("${breed}s bark")
+    }
+}
+//call the func by creating object
+fun main()
+{
+    val f1=First()
+    f1.breed="German Stephard"
+    f1.color="Black"
+    f1.age=1
+    println("The breed of the dog is ${f1.breed},whose color is ${f1.color} ,and age is ${f1.age}")
+    f1.eat()
+    f1.bark()
+}
